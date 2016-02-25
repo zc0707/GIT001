ba064ca33d0d5313c9cd96369ccda17984865fcd
zc0707 <876757288@qq.com>
2016-02-25 23:22:08 +0800

    update 02
    update 02

--- src/com/atguigu/git/test/Hello.java
+++ src/com/atguigu/git/test/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("欢迎来到硅谷0917");
 		System.out.println("欢迎来到硅谷0918");
+		System.out.println("欢迎来到硅谷0919");
 	}
 	
 }
